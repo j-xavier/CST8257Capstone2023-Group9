04c8cf3d81221c64f2387fc3ef74a5dcf04203f7
j-xavier <johnnyboyxavier735@gmail.com>
2023-07-20 14:57:58 -0400

    added model, factory, and resource

---
+++ app/Http/Controllers/TasklistController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tasklist;
+use Illuminate\Http\Request;
+
+class TasklistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tasklist $tasklist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tasklist $tasklist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tasklist $tasklist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tasklist $tasklist)
+    {
+        //
+    }
+}
