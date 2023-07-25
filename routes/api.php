729fa1259ab2031c6d026ea64b22809e931b1517
j-xavier <johnnyboyxavier735@gmail.com>
2023-07-25 13:39:23 -0400

    installed sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TasklistController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,13 +9,11 @@ use App\Http\Controllers\TasklistController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::get('/', [TaskListController::class, 'index']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
